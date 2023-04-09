5f348de0770163307b4c5044140ccc308b38c624
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-04-09 23:12:16 +0100

    Basic styling for Testimonial section done

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,16 +1,21 @@
+import { Card, Flex, Grid, GridItem, Heading } from '@chakra-ui/react'
 import React from 'react'
 
 const Testimonials = () => {
   return (
-    <div className='testimonials'>
-      <h3>Testimonials</h3>
-      <section>
-        card 1 <br />
-        card 2 <br />
-        card 3 <br />
-        card 4 <br />
-      </section>
-    </div>
+    <Grid className='testimonials' templateColumns="repeat(12, 1fr)" pt={10} pb={10}  bg="#BEBEBE">
+      <GridItem colStart={3} colSpan={8}>
+        <Flex flexDir='column'>
+          <Heading as="h3" fontSize="16pt" alignSelf="center" mb={7}>Testimonials</Heading>
+          <Flex justify='space-between'>
+            <Card>card 1</Card>
+            <Card>card 2</Card>
+            <Card>card 3</Card>
+            <Card>card 4</Card>
+          </Flex>
+        </Flex>
+      </GridItem>
+    </Grid>
   )
 }
 
