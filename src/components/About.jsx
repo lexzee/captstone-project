04d0826d25c9e000e3638970e7d96b7c3a6bbca4
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-04-11 00:20:04 +0100

    basic styling of About section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import img1 from '../assets/images/Mario_Adrian_A.jpg'
 import img2 from '../assets/images/Mario_Adrian_b.jpg'
+import { Grid, GridItem, Heading, Text } from '@chakra-ui/react'
 
 const About = () => {
   return (
-    <div id='about' className='about'>
-      <section>
-        <div>
-          <h1>Little Lemon</h1>
-          <p>Chicago</p>
-        </div>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </section>
-      <section>
-        <img src={img1} alt="" style={{width: '50%'}}/>
-        <img src={img2} alt="" style={{width: '50%'}}/>
-      </section>
-    </div>
+    <Grid id='about' className='about' templateColumns="repeat(12, 1fr)" pt={10} pb={10} h="25rem">
+      <GridItem colStart={3} colSpan={8}>
+        <Grid templateColumns="repeat(2, 1fr)" gap={5} position='relative'>
+          <GridItem color="black" position="relative">
+            <Grid align="left" rowGap='2rem' style={{position: 'absolute',top:'50%', bottom: '50%'}}>
+            <GridItem>
+              <Heading color="#F4CE14">Little Lemon</Heading>
+              <Text>Chicago</Text>
+            </GridItem>
+            <GridItem>
+              <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </Text>
+            </GridItem>
+            </Grid>
+          </GridItem>
+          <GridItem position="relative" height="20">
+            <img src={img1} alt="" style={{width: '15rem',height: '15rem', position: 'absolute', top: '100%', left:'0', zIndex: '1', borderRadius:'16px'}}/>
+            <img src={img2} alt="" style={{width: '15rem',height: '15rem', position: 'absolute', top: '0', right: '0', zIndex: '0', borderRadius:'16px'}}/>
+          </GridItem>
+        </Grid>
+      </GridItem>
+    </Grid>
   )
 }
 
