0a2f624a23c83cb93ff4f32cabb3b8447f411113
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-04-11 12:30:33 +0100

    basic form styling for reserve a table

--- src/components/ReserveTable.jsx
+++ src/components/ReserveTable.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import { Grid, GridItem, Input, InputGroup, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Spacer } from '@chakra-ui/react'
+import { Grid, GridItem, Heading, Input, InputGroup, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Flex, VStack } from '@chakra-ui/react'
 
 const ReserveTable = () => {
+  const fieldset = {
+    // border: 'none',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start',
+    justifyContent: 'space-between',
+  }
   return (
     <>
       <Header />
       <Grid templateColumns="repeat(12, 1fr)">
         <GridItem colStart={3} colSpan={8}>
           <form>
-            <fieldset>
-              <header>Reserve a Table</header>
+            <fieldset style={fieldset}>
+              <Heading as="h1" alignSelf='center'>Reserve a Table</Heading>
               <label for='name'>Name:</label>
               <Input placeholder='Enter your full name' name="name" />
               <label for='date'>Date and Time:</label>
               <Input placeholder='Select Date and Time' type="datetime-local" />
-              <InputGroup>
+              <VStack align="left" position="relative">
                 <label for='noOfDiner'>Number of Diner</label>
-                <NumberInput defaultValue={0} min={1} max={20}>
+                <NumberInput defaultValue={0} min={1} max={20} display="flex">
                   <NumberInputField />
                   <NumberInputStepper>
                     <NumberIncrementStepper />
                     <NumberDecrementStepper />
                   </NumberInputStepper>
                 </NumberInput>
-              </InputGroup>
+              </VStack>
               <label for='occassion'>Select Occassion:</label>
               <Input placeholder='Enter your full name' name="occassion" />
               <label for='location'>Select Location:</label>
