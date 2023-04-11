d9ba14baf9bdc4f29c212921f9367eb6629eb29b
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-04-11 01:04:08 +0100

    Basic Styling of f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,44 @@
+import { Flex, Grid, GridItem, HStack, Text } from '@chakra-ui/react';
 import logo from '../assets/Logo.svg';
 
 const Footer = () => {
   return (
-    <footer>
-      <img src={logo} alt="Little lemon's logo" />
-      <aside>
-        <h4>Doormat Navigation</h4>
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">Menu</a></li>
-          <li><a href="">Reservations</a></li>
-          <li><a href="">Order online</a></li>
-          <li><a href="">Login</a></li>
-        </ul>
-      </aside>
-      <aside>
-        <h4>Contact Info</h4>
-        <ul>
-          <li><a href="">Address</a></li>
-          <li><a href="">Phone Number</a></li>
-          <li><a href="">Email</a></li>
-        </ul>
-      </aside>
-      <aside>
-        <h4>Social Media Links</h4>
-        <ul>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">Instagram</a></li>
-          <li><a href="">Twitter</a></li>
-        </ul>
-      </aside>
-    </footer>
+    <Grid as="footer" gridTemplateColumns="repeat(12, 1fr)" pt={10} pb={10} fontSize={'sm'} bg="#D9D9D9">
+      <GridItem colStart={3} colSpan={8}>
+        <Grid gap={10} templateColumns="repeat(4,1fr)">
+          <GridItem position="relative">
+            <img src={logo} alt="Little lemon's logo" style={{position: 'absolute', top: '40%', bottom: '40%'}}/>
+          </GridItem>
+          <GridItem>
+            <Text fontWeight="extrabold">Doormat Navigation</Text>
+            <Flex flexDir="column">
+              <a href="">Home</a>
+              <a href="">About</a>
+              <a href="">Menu</a>
+              <a href="">Reservations</a>
+              <a href="">Order online</a>
+              <a href="">Login</a>
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <Text fontWeight="extrabold">Contact Info</Text>
+            <Flex flexDir="column">
+              <a href="">Address</a>
+              <a href="">Phone Number</a>
+              <a href="">Email</a>
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <Text fontWeight="extrabold">Social Media Links</Text>
+            <Flex flexDir="column">
+              <a href="">Facebook</a>
+              <a href="">Instagram</a>
+              <a href="">Twitter</a>
+            </Flex>
+          </GridItem>
+        </Grid>
+      </GridItem>
+    </Grid>
   )
 }
 
